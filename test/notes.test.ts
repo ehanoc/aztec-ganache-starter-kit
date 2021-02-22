758a367b20fcca50e646189a591c39da617be9dc
ehanoc <bmartins.6018@gmail.com>
2021-02-22 16:08:13 +0000

    cleanup

--- test/notes.test.ts
+++ test/notes.test.ts
@@ -19,42 +19,7 @@ const ACE = artifacts.require("./ACE.sol")
 const stripPrependedZeroes = (str) => str.replace(/^0{1,}/, '');
 
 const Web3 = require('web3')
-const { toChecksumAddress } = require('web3-utils')
 const web3 = new Web3(new Web3.providers.WebsocketProvider("ws://localhost:8545"))
-
-const abiDecoder = require('abi-decoder')
-const noteDataTypes = ['bytes32', 'uint256']
-
-const ADDRESS_LENGTH = 40;
-
-const USER_PUBLIC_KEY_LENGTH = 66;
-const EXTENSION_PUBLIC_KEY_LENGTH = 64;
-
-const REAL_VIEWING_KEY_LENGTH = 138;
-const VIEWING_KEY_LENGTH = 420;
-
-const METADATA_AZTEC_DATA_LENGTH = 194;
-const AZTEC_JS_METADATA_PREFIX_LENGTH = 130;
-const AZTEC_JS_DEFAULT_METADATA_PREFIX_LENGTH = 66;
-
-const DYNAMIC_VAR_CONFIG_LENGTH = 64;
-const MIN_BYTES_VAR_LENGTH = 64;
-
-const START_EVENTS_SYNCING_BLOCK = 0;
-const metadataConfig = [
-  {
-    name: 'addresses',
-    length: ADDRESS_LENGTH,
-    _toString: toChecksumAddress,
-  },
-  {
-    name: 'viewingKeys',
-    length: VIEWING_KEY_LENGTH,
-  },
-  {
-    name: 'appData',
-  },
-]
 
 const {
   proofs: { MINT_PROOF }
@@ -72,193 +37,145 @@ contract("Private payment", accounts => {
 
   const observer = secp256k1.accountFromPrivateKey(process.env.GANACHE_TESTING_ACCOUNT_2)
 
+  const observerWithNoAccess = secp256k1.accountFromPrivateKey(process.env.GANACHE_TESTING_ACCOUNT_3)
+
+  // Curve25519 Keys
+  // Observer needs to remember this, per note
+  const observerKeyPairCurve25519 = nacl.box.keyPair()
+  const observerCurve25519PubKey = '0x' + Buffer.from(observerKeyPairCurve25519.publicKey).toString('hex')
+
   let privatePaymentContract;
   let aceContract;
   let web3ZkAssetContract
 
-  beforeEach(async () => {
-    privatePaymentContract = await ZkAssetMintable.deployed();
-    aceContract = await ACE.deployed()
+  let noteEventMinted
 
-    web3ZkAssetContract = new web3.eth.Contract(privatePaymentContract.abi, privatePaymentContract.address)
-  });
+  const noteValue = 100
+  let mintedNote
 
-  it('[MINT][SENDER] should to able to read note event data', async () => {
-    const receipt = await mintSample(privatePaymentContract, accounts, bob.publicKey, 100, [])
-    const logs = receipt.logs
-    const createNoteEvent = logs[0]
-    const noteHash = createNoteEvent.args.noteHash
-    expect(createNoteEvent.event).to.equal('CreateNote')
+  // Deploys, gives access to note, listens to event of the mint so that we can run the tests on independently
+  before(done => {
+    if (!privatePaymentContract) {
+      ZkAssetMintable.deployed().then(contract => {
+        privatePaymentContract = contract
+        ACE.deployed().then(contract => {
+          aceContract = contract
+          web3ZkAssetContract = web3ZkAssetContract ? web3ZkAssetContract : new web3.eth.Contract(privatePaymentContract.abi, privatePaymentContract.address)
 
-    console.log(`createNoteEvent: ${JSON.stringify(createNoteEvent)}`)
-    console.log(`metadata: ${createNoteEvent.args.metadata}`)
-    const decoded = web3.eth.abi.decodeLog(noteDataTypes, createNoteEvent.args.metadata)
+          web3ZkAssetContract.events.allEvents()
+            .on('data', (event) => {
+              if (event.event === 'CreateNote') {
+                noteEventMinted = event
 
-    const ephemeralKey = decoded[0]
-    expect(ephemeralKey).to.be.a('string')
-    expect(ephemeralKey).to.have.length(66)
+              }
+            }).on('error', console.error)
 
-    const approvedAddressesOffset = decoded[1]
-    expect(approvedAddressesOffset).to.be.a('string')
+          // [IMPORTANT]
+          // Key Exchange between Observer and Minter needed
+          // Minter needs to know what Curve25519 key to link to the note so that it can be decrypted
+          //only by the observer
+          const observers = [
+            {
+              address: observer.address,
+              linkedPublicKey: observerCurve25519PubKey
+            }
+          ]
 
-    console.log(`decoded: ${JSON.stringify(decoded)}`)
+          mintNote(privatePaymentContract, accounts, bob.publicKey, noteValue, observers).then(note => {
+            mintedNote = note
+            done()
+          })
+        })
+      })
+    }
 
-    const fetchedNote = await aceContract.getNote(privatePaymentContract.address, noteHash)
-    console.log(`fetched note: ${JSON.stringify(fetchedNote)}`)
-
-    // const encryptedViewKeysOffset = decoded[2]
   })
 
-  it('[SENDER][NOTE] should be able get note value and key', async () => {
-    const noteValue = 100
-    const bobNote1 = await aztec.note.create(bob.publicKey, noteValue);
-    const viewNote = bobNote1.getView()
+  it('A Note was minted!', async () => {
+    expect(mintedNote).to.not.equal(undefined)
+  })
 
-    console.log(`viewNote: ${viewNote}`)
+  it('Should be able to rebuild note only from event / public data', async () => {
+    const metadata = noteEventMinted.returnValues.metadata
+    // Only 196 first chars (including 0x); the rest of metadata is discarded here
+    const noteFromEventLog = await aztec.note.fromEventLog(metadata.slice(0, 0xc4))
+
+    // x & y comparing
+    expect(noteFromEventLog.sigma[0]).to.equal(mintedNote.sigma[0])
+    expect(noteFromEventLog.sigma[1]).to.equal(mintedNote.sigma[1])
+
+    expect(noteFromEventLog.gamma[0]).to.equal(mintedNote.gamma[0])
+    expect(noteFromEventLog.gamma[1]).to.equal(mintedNote.gamma[1])
+
+    // Sigma & Gamma are points on a bn128 ecc curve that point to the noteHash and NoteValue
+  })
+
+  it('Should NOT be able to derive a (noteHash) & k (quantity) from just a note event emitted', async () => {
+    const metadata = noteEventMinted.returnValues.metadata
+    // Only 196 first chars (including 0x); the rest of metadata is discarded here
+    const noteFromEventLog = await aztec.note.fromEventLog(metadata.slice(0, 0xc4))
+    const viewNote = noteFromEventLog.getView()
+
     const a = viewNote.substr(2, 64)
     const k = viewNote.substr(66, 8)
     const ephemeralKey = viewNote.substr(74, 66)
 
-    expect(parseInt(k, 16)).to.equal(noteValue)
-
-    console.log(`a : ${parseInt(a, 16)}, k: ${parseInt(k, 16)}`)
-    console.log(`ephemeral key : ${ephemeralKey}`)
+    expect(a).to.equal('')
+    expect(k).to.equal('')
+    expect(ephemeralKey).to.be.a('string')
+    expect(ephemeralKey).to.have.lengthOf(0)
   })
 
-  it('Export Note', async () => {
-    const bobNote1 = await aztec.note.create(bob.publicKey, 100);
-    const exportNote = bobNote1.exportNote()
+  it('As an observer with access, i should have access to note with viewing key ', async () => {
+    const metadata = noteEventMinted.returnValues.metadata
+    const noteFromEventLog = await aztec.note.fromEventLog(metadata.slice(0, 0xc4))
 
-    console.log(`exportNote: ${JSON.stringify(exportNote)}`)
+    // the rest
+    const eventMetadataKeyStore = metaDataConstructor(metadata.slice(0xc4))
+
+    const allowedAccess = eventMetadataKeyStore.getAccess(observer.address)
+    const encryptedViewingKey = allowedAccess.viewingKey
+    const ViewKey = decodeSecretBoxFrom(encryptedViewingKey)
+
+    const decryptedViewingKey = decrypt(observerKeyPairCurve25519.secretKey, ViewKey.nonce, ViewKey.ephemeralPublicKey, ViewKey.chiperText)
+
+    const decryptedNote = await aztec.note.fromViewKey(decryptedViewingKey)
+    expect(decryptedNote.k.toNumber()).to.equal(noteValue)
   })
 
-  it.skip('[SENDER] Adds access to third party ', async () => {
-    // const web3ZkAssetContract = new web3.eth.Contract(privatePaymentContract.abi, privatePaymentContract.address)
+  it('As an observer with NO access, i should have access to note with viewing key ', async () => {
+    const metadata = noteEventMinted.returnValues.metadata
+    const noteFromEventLog = await aztec.note.fromEventLog(metadata.slice(0, 0xc4))
 
-    //  curve25519 pair
-    const observerKeyPairCurve25519 = nacl.box.keyPair()
-    const observerPubKey = '0x' + Buffer.from(observerKeyPairCurve25519.publicKey).toString('hex')
-    console.log(`observerPubKey: ${observerPubKey}`)
+    // the rest
+    const eventMetadataKeyStore = metaDataConstructor(metadata.slice(0xc4))
 
-    const mintNote = await mintSample(privatePaymentContract, accounts, bob.publicKey, 100, [
-      {
-        address: observer.address, //eth addr
-        linkedPublicKey: observerPubKey //pub key curve25519 ephemeral 
-      }
-    ])
-
-    console.log(`metadata note : ${mintNote.metaData}`)
-
-    const metaObj = metaDataConstructor(
-      mintNote.metaData.slice(
-        66 + 2
-      )
-    )
-
-    const allowedAccess = metaObj.getAccess(observer.address)
-    console.log(`allowedAccess: ${allowedAccess}`)
-
-
-    console.log(`metaObj: ${JSON.stringify(metaObj)}`)
+    const allowedAccess = eventMetadataKeyStore.getAccess(observerWithNoAccess.address)
+    expect(allowedAccess).to.equal(null)
   })
 
-  it.only('[THIRD-PARTY] should have access to note with viewing key', async () => {
-    web3ZkAssetContract.events.allEvents()
-      .on('data', async (event) => {
-        if (event.event === 'CreateNote') {
-          const metadata = event.returnValues.metadata
-
-          const noteFromEventLog = await aztec.note.fromEventLog(metadata.slice(0, 196))
-          console.log(`noteFromEventLog: ${JSON.stringify(noteFromEventLog)}`)
-
-          const prefix_metadata_length = AZTEC_JS_METADATA_PREFIX_LENGTH + AZTEC_JS_DEFAULT_METADATA_PREFIX_LENGTH
-
-          const noteKeyStore = metadata
-
-          const eventMetadataKeyStore = metaDataConstructor(
-            noteKeyStore.slice(prefix_metadata_length)
-          )
-          console.log(`eventMetadataKeyStore: ${JSON.stringify(eventMetadataKeyStore)}`)
-
-          const allowedAccess = eventMetadataKeyStore.getAccess(observer.address)
-          const encryptedViewingKey = allowedAccess.viewingKey
-          console.log(`encryptedViewingKey: ${encryptedViewingKey}, len: ${encryptedViewingKey.length}`)
-
-          const NonceLength = 48 + 2 // + 2 for 0x prefix
-          const EphemeralPublicKeyLength = 64
-          const ViewKey = {
-            nonce: encryptedViewingKey.slice(0, NonceLength),
-            ephemeralPublicKey: `0x${encryptedViewingKey.slice(NonceLength, NonceLength + EphemeralPublicKeyLength)}`,
-            chiperText: `0x${encryptedViewingKey.slice(NonceLength + EphemeralPublicKeyLength)}`
-          }
-
-          const decryptedViewingKey = decrypt(observerKeyPairCurve25519.secretKey, ViewKey.nonce, ViewKey.ephemeralPublicKey, ViewKey.chiperText)
-          console.log(`decryptedViewingKey: ${decryptedViewingKey}`)
-
-          const decryptedNote = await aztec.note.fromViewKey(decryptedViewingKey)
-          console.log(decryptedNote)
-          expect(decryptedNote.k.toNumber()).to.equal(100)
-          console.log(`decrypted note value : ${decryptedNote.k.toNumber()}`)
-          
-
-          // const accessObj = getAccessNoteMetadata(metaObj, observer.address)
-          // console.log(`accessObj : ${JSON.stringify(accessObj)}`)
-        }
-      })
-      .on('error', console.error);
-
-    const observerKeyPairCurve25519 = nacl.box.keyPair()
-    const observerPubKey = '0x' + Buffer.from(observerKeyPairCurve25519.publicKey).toString('hex')
-
-    console.log(`observer : ${JSON.stringify(observer)}`)
-
-    const bobNote1 = await mintSample(privatePaymentContract, accounts, bob.publicKey, 100, [
-      {
-        address: observer.address,
-        linkedPublicKey: observerPubKey
-      }
-    ])
-
-    // console.log(`###################`)
-    // console.log(`minted note : ${JSON.stringify(bobNote1)}`)
-
-    // console.log(`##########################################################`)
-    // const view = bobNote1.getView()
-
-    // const derived = await aztec.note.fromViewKey(view)
-    // console.log(`derived: ${JSON.stringify(derived)}`)
-
-
-    // const metaDataPrefix = bobNote1.metaData.slice(
-    //   0,
-    //   AZTEC_JS_DEFAULT_METADATA_PREFIX_LENGTH + 2
-    //  )
-
-    // console.log(`metaDataPrefix: ${metaDataPrefix}`)
-
-    // const newMetaData =
-    //   metaDataPrefix + bobNote1.exportMetaData().slice(2)
-    // console.log(`newMetaData: ${newMetaData}`)
-
-    // const prefix_metadata_length = AZTEC_JS_METADATA_PREFIX_LENGTH + AZTEC_JS_DEFAULT_METADATA_PREFIX_LENGTH
-
-    // const metaObj = metaDataConstructor(
-    //   newMetaData.slice(
-    //     prefix_metadata_length
-    //   )
-    // )
-
-    // console.log(`metaObj: ${JSON.stringify(metaObj)}`)
+  it('Why i cant rebuild the owner? ', async() => {
+    const metadata = noteEventMinted.returnValues.metadata
+    const noteFromEventLog = await aztec.note.fromEventLog(metadata.slice(0, 0xc4))
+    expect(noteFromEventLog.owner).to.equal('0x') // hmm
   })
-});
+})
 
-// returns note
-const mintSample = async (contract, accounts, publicKey, value, viewers) => {
-  const bobNote1 = await aztec.note.create(publicKey, value, viewers, undefined);
+// simply mints and returns note
+/**
+ * 
+ * @param contract 
+ * @param accounts 
+ * @param publicKey 
+ * @param value 
+ * @param viewers - map who etherem address => ephemeral 25519 pub key which has access (by generating the symmetric key offline) 
+ */
+const mintNote = async (contract, accounts, publicKey, value, viewers) => {
+  const bobNote = await aztec.note.create(publicKey, value, viewers, undefined);
   const newMintCounterNote = await aztec.note.create(publicKey, value, undefined);
   const zeroMintCounterNote = await aztec.note.createZeroValueNote();
   const sender = accounts[0];
-  const mintedNotes = [bobNote1];
+  const mintedNotes = [bobNote];
 
   const mintProof = new MintProof(
     zeroMintCounterNote,
@@ -273,37 +190,52 @@ const mintSample = async (contract, accounts, publicKey, value, viewers) => {
     from: accounts[0]
   })
 
-  const viewNote = bobNote1.getView()
-  console.log(`viewNote: ${viewNote}`)
-  const a = viewNote.substr(2, 64)
-  const k = viewNote.substr(66, 8)
-  const ephemeralKey = viewNote.substr(74, 66)
-
-  // console.log(`adding access to : ${JSON.stringify(viewers)}`)
-  // viewNote.grantViewAccess(viewers)
-
-  console.log(`a : ${parseInt(a, 16)}, k: ${parseInt(k, 16)}`)
-  console.log(`ephemeral key : ${ephemeralKey}`)
-
-  console.log(`bobNote: ${JSON.stringify(bobNote1)}`)
-
-  return bobNote1
+  return bobNote
 }
 
-function getAccessNoteMetadata(metadata, address) {
-  const { addresses, viewingKeys } = metadata;
+/**
+ * 
+ * @param viewingKey - This is a Curve25519 boxed payload which includes the necessary params to decrypt, if ECDH
+ * can be performed from the same 25519 linkedPubKey when access was given to a note. 
+ * 
+ * The encrypted chiper text is the ECDH secret, which is a symmetric encryption / decription key. THAT IS THE ACTUAL VIEWING KEY
+ */
+function decodeSecretBoxFrom(viewingKey) {
+  //https://github.com/dchest/tweetnacl-js/blob/master/README.md#constants
+  /**
+   * nacl.box.publicKeyLength = 32
+      Length of public key in bytes.
 
-  const idx = addresses.findIndex((a) => a === toChecksumAddress(address));
-  if (idx < 0) {
-    return null;
-  }
+      nacl.box.secretKeyLength = 32
+      Length of secret key in bytes.
 
+      nacl.box.sharedKeyLength = 32
+      Length of precomputed shared key in bytes.
+
+      nacl.box.nonceLength = 24
+      Length of nonce in bytes.
+
+      nacl.box.overheadLength = 16
+      Length of overhead added to box compared to original message.
+   * 
+   * 
+   */
+  // 24 bytes * 2 (2 char per byte in a string) + 2 for 0x prefix
+  const nonceLength = 24 * 2 + 2 
+
+
+   // 32 bytes * 2 (2 char per byte)
+  const publicKey25519Length = 32 * 2
+
+  // Counter part generated in @aztec/note-access/src/crypto/encryptMessage.js
   return {
-    address: addresses[idx],
-    viewingKey: viewingKeys[idx],
-  };
+    nonce: viewingKey.slice(0, nonceLength),
+    ephemeralPublicKey: '0x' + `${viewingKey.slice(nonceLength, nonceLength + publicKey25519Length)}`,
+    chiperText: '0x' + `${viewingKey.slice(nonceLength + publicKey25519Length)}`
+  }
 }
 
+//
 function decrypt(receiverPrivateKeyUint8Array, nonce, ephemeralPublicKey, cipherText) {
   let decryptedMessage
   try {
@@ -324,6 +256,6 @@ function decrypt(receiverPrivateKeyUint8Array, nonce, ephemeralPublicKey, cipher
   if (!decryptedMessage) {
     throw Error(`Failed to decrypt viewing key with private key.`)
   }
-  this._viewingKey = "0x" + tweetNaclUtils.encodeUTF8(decryptedMessage)
-  return this._viewingKey
+
+  return "0x" + tweetNaclUtils.encodeUTF8(decryptedMessage)
 }
